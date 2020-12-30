2f55e0f65bb3935a648a5f534db28715964b19ea
Sangheon Lee <wwdbsh@gmail.com>
2020-12-30 14:30:11 -0500

    20201230:update

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import AppLoading from "expo-app-loading";
+import { Text, View } from 'react-native';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo-hooks";
+import AsyncStorage from "@react-native-community/async-storage";
+import options from "./apollo";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+  const preLoad = async () => {
+    try{
+      await Font.loadAsync({...Ionicons.font});
+      await Asset.loadAsync([require("./assets/logo.png")]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage:AsyncStorage,
+      });
+      const client = new ApolloClient({
+        cache,
+        ...options
+      });
+      setLoaded(true);
+      setClient(client);
+    }catch(e){
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    preLoad();
+  }, []);
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </ApolloProvider>
+    ): (
+      <AppLoading />
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
